658a9ebcee4263f7129066757995f51de7c1b5de
r4khul <blackempire9177@gmail.com>
2025-12-23 00:43:36 +0530

    chore(main): all preq for bidings in main func

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:compound/features/habit/data/models/habit_model.dart';
 import 'package:compound/features/habit/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(HabitModelAdapter());
+
   runApp(CompoundApp());
 }
 
